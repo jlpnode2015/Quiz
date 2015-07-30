6bf1a8aef7413a267b8d2e6103e4425be412e4c3
jlpnode2015 <jlpnode2015@gmail.com>
2015-07-30 20:34:59 +0200

    Quiz
    Proyecto Quiz

---
+++ models/quiz.js
@@ -0,0 +1,10 @@
+//Definicion del módulo de quiz
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Quiz',
+	{
+	  pregunta: DataTypes.STRING,
+	  respuesta: DataTypes.STRING,
+	});
+}
+
